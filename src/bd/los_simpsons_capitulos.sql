8addffca7592ebcdc7455e2a87bc6b2391d20932
Sandra <sandra_13_c70@hotmail.com>
2025-07-31 21:49:22 +0200

    subiendo bbdd sql

---
+++ src/bd/los_simpsons_capitulos.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: los_simpsons
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `capitulos`
+--
+
+DROP TABLE IF EXISTS `capitulos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `capitulos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `numero_episodio` int DEFAULT NULL,
+  `temporada` int DEFAULT NULL,
+  `fecha_emision` date DEFAULT NULL,
+  `sinopsis` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `capitulos`
+--
+
+LOCK TABLES `capitulos` WRITE;
+/*!40000 ALTER TABLE `capitulos` DISABLE KEYS */;
+INSERT INTO `capitulos` VALUES (1,'Bart Gets an F',1,2,'1990-10-11','Bart intenta aprobar el examen final para no repetir curso'),(2,'Lisa the Vegetarian',5,7,'1995-10-15','Lisa decide dejar de comer carne tras visitar una granja'),(3,'Homer vs. Lisa and the 8th Commandment',13,2,'1991-02-07','Lisa cuestiona la moralidad de robar cable'),(4,'Treehouse of Horror V',6,6,'1994-10-30','Especial de Halloween con tres historias de terror'),(5,'Mr. Plow',9,4,'1992-11-19','Homer inicia un negocio de quitanieves');
+/*!40000 ALTER TABLE `capitulos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-31 21:47:48
---
+++ src/bd/los_simpsons_frases.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: los_simpsons
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `frases`
+--
+
+DROP TABLE IF EXISTS `frases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `frases` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `texto` varchar(100) NOT NULL,
+  `marca_tiempo` varchar(10) DEFAULT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
+  `fk_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id` (`fk_id`),
+  CONSTRAINT `frases_ibfk_1` FOREIGN KEY (`fk_id`) REFERENCES `personajes` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `frases`
+--
+
+LOCK TABLES `frases` WRITE;
+/*!40000 ALTER TABLE `frases` DISABLE KEYS */;
+INSERT INTO `frases` VALUES (1,'¡D\'oh!','00:01','Expresión clásica de frustración de Homer',1),(2,'¡Ay, caramba!','00:03','Frase típica de Bart ante el caos',3),(3,'Excelente...','00:05','Susurro malévolo de Mr. Burns',5),(4,'¡Multiplícate por cero!','00:07','Insulto absurdo de Bart',3),(5,'¿Y si nos equivocamos de religión? Dios estaría más furioso cada semana.','00:10','Reflexión absurda de Homer',1),(6,'Los vampiros son seres inventados, como los duendes, los gremlins y los esquimales.','00:12','Confusión total de Homer',1);
+/*!40000 ALTER TABLE `frases` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-31 21:47:48
